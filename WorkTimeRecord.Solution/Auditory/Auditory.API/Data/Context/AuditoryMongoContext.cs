1f419369d11e75badf11e0669bd2aa8847eb40a7
Ramón Serrano Valero <ramon.serrano.valero@gmail.com>
2025-01-30 08:40:20 +0100

    Initial Commit

---
+++ WorkTimeRecord.Solution/Auditory/Auditory.API/Data/Context/AuditoryMongoContext.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+
+namespace Auditory.API.Data.Context
+{
+    public class AuditoryMongoContext : IAuditoryContext
+    {
+        private readonly IConfiguration _configuration;
+        private IMongoDatabase _mongoDatabase;
+        public AuditoryMongoContext(IConfiguration configuration)
+        {
+            _configuration = configuration;            
+            _mongoDatabase = new MongoClient(string.Concat(
+                                                 "mongodb://",
+                                                configuration.GetConnectionString("UserRecord:Username"), ":",
+                                                configuration.GetConnectionString("UserRecord:Password"), "@",
+                                                configuration.GetConnectionString("UserRecord:HostName"), ":",
+                                                configuration.GetConnectionString("UserRecord:Port"),
+                                                "/?authSource=admin")                                                
+                                            ).GetDatabase(configuration.GetConnectionString("UserRecord:DataBase"));
+
+        }
+        public IMongoCollection<UserRecordHistoryMongo> UserRecordCollection =>
+            _mongoDatabase.GetCollection<UserRecordHistoryMongo>(_configuration.GetConnectionString("UserRecord:Collection"));
+
+    }
+}
---
+++ WorkTimeRecord.Solution/Components/Components.Communication/MessageBroker/ExtendServices.cs
@@ -0,0 +1,41 @@
+﻿using Components.Communication.MessageBroker.Configuration;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Components.Communication.MessageBroker
+{
+    public static class ExtendServices
+    {
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services
+                                                             , IConfiguration configuration
+                                                             , Assembly? assembly=null)
+        {            
+            var rabbitMqOptions = new RabbitMqHostOptions();
+            configuration.GetSection("MessageBroker").Bind(rabbitMqOptions);
+            Console.WriteLine($"RabbitMQ Host: {rabbitMqOptions.Host}");
+            Console.WriteLine($"RabbitMQ User: {rabbitMqOptions.UserName}");
+            Console.WriteLine($"RabbitMQ Password: {rabbitMqOptions.Password}");
+
+            services.AddMassTransit(busConfiguration => {
+
+                busConfiguration.SetKebabCaseEndpointNameFormatter();
+                
+                if (assembly != null)
+                    busConfiguration.AddConsumers(assembly);
+
+                busConfiguration.UsingRabbitMq((context, rabbitMQconfiguration) => {
+                    rabbitMQconfiguration.Host(new Uri(rabbitMqOptions.Host), host =>
+                    {
+                        host.Username(rabbitMqOptions.UserName);
+                        host.Password(rabbitMqOptions.Password);
+                    });                    
+                    rabbitMQconfiguration.ConfigureEndpoints(context);
+                });
+
+            });
+            return services;
+        }
+    }
+}
